f2f82e0b8acc7c8656a90c8f5a939439a105d3c9
Harshad-Sahu <harshadsahu17@gmail.com>
2023-12-29 14:43:58 +0530

    code cleanup-1

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,4 @@ export * from "./Headers";
 export * from "./Filters";
 export * from "./Loader";
 export * from "./YearlyMovie";
-export * from "./VirtualisedYearlyList";
-export * from "./VirtualisedGridSearch";
 export * from "./NoData";
